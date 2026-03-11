a4337f6cfb2b068f2bb0ef20c703843ac3ca339d
ahsankabirr <mdahsankabirrazu@gmail.com>
2026-03-11 12:25:27 +0200

    add redirect on JS file

---
+++ js/redirect.js
@@ -0,0 +1,10 @@
+// Donation direction
+document.getElementById("donation-Btn").addEventListener("click", (event) => {
+  console.log("Donation Btn Working");
+  window.location.href = "./index.html";
+});
+// History direction
+document.getElementById("history-Btn").addEventListener("click", (event) => {
+  console.log("history Btn Btn Working");
+  window.location.href = "./history.html";
+});
